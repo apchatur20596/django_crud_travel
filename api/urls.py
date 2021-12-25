ec5aa8589f93bdeafa71d801328c4909a4137d4e
Ajinkya Chatur <ajinkya.chatur20596@gmail.com>
2021-12-25 02:13:46 -0800

    update and delete api functions added

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import travelplans_list
+from .views import travelplans_list, travelplans_details
 from .views import Index
 
 
 urlpatterns = [
     path('', Index),
     path('travelplans/', travelplans_list),
+    path('travelplans/<int:pk>/', travelplans_details)
 ]
--- api/views.py
+++ api/views.py
@@ -1,26 +1,52 @@
 from django.shortcuts import render, HttpResponse
 from .models import TravelPlans
 from .serializers import TravelPlansSerializer
-from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
 
 def Index(request):
     return HttpResponse("It is working")
 
+@api_view(['GET', 'POST'])
 def travelplans_list(request):
 
     #get all travel plans
     if request.method == 'GET':
         travelplans = TravelPlans.objects.all()
         serializer = TravelPlansSerializer(travelplans, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TravelPlansSerializer(data=data)
+        serializer = TravelPlansSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status = 201)
-        return JsonResponse(serializer.errors, status = 400)    
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)   
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def travelplans_details(request, pk):
+    try:
+        travelplan = TravelPlans.objects.get(pk=pk)
+
+    except TravelPlans.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = TravelPlansSerializer(travelplan)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TravelPlansSerializer(travelplan, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        travelplan.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
